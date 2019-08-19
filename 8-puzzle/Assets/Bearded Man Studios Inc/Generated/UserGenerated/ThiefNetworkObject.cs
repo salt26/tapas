1312d82a63cf38634f4415e0a3faba1a1bac6971
pzmaus <pzmaus@snu.ac.kr>
2019-08-19 17:58:50 +0900

    tutorial game added

--- 8-puzzle/Assets/Bearded Man Studios Inc/Generated/UserGenerated/ThiefNetworkObject.cs
+++ 8-puzzle/Assets/Bearded Man Studios Inc/Generated/UserGenerated/ThiefNetworkObject.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 namespace BeardedManStudios.Forge.Networking.Generated
 {
-	[GeneratedInterpol("{\"inter\":[0.15,0.15]")]
+	[GeneratedInterpol("{\"inter\":[0.15,0.15,0.15,0.15,0,0]")]
 	public partial class ThiefNetworkObject : NetworkObject
 	{
-		public const int IDENTITY = 9;
+		public const int IDENTITY = 10;
 
 		private byte[] _dirtyFields = new byte[1];
 
@@ -77,6 +77,130 @@ namespace BeardedManStudios.Forge.Networking.Generated
 			if (rotationChanged != null) rotationChanged(_rotation, timestep);
 			if (fieldAltered != null) fieldAltered("rotation", _rotation, timestep);
 		}
+		[ForgeGeneratedField]
+		private float _m_Horizontal;
+		public event FieldEvent<float> m_HorizontalChanged;
+		public InterpolateFloat m_HorizontalInterpolation = new InterpolateFloat() { LerpT = 0.15f, Enabled = true };
+		public float m_Horizontal
+		{
+			get { return _m_Horizontal; }
+			set
+			{
+				// Don't do anything if the value is the same
+				if (_m_Horizontal == value)
+					return;
+
+				// Mark the field as dirty for the network to transmit
+				_dirtyFields[0] |= 0x4;
+				_m_Horizontal = value;
+				hasDirtyFields = true;
+			}
+		}
+
+		public void Setm_HorizontalDirty()
+		{
+			_dirtyFields[0] |= 0x4;
+			hasDirtyFields = true;
+		}
+
+		private void RunChange_m_Horizontal(ulong timestep)
+		{
+			if (m_HorizontalChanged != null) m_HorizontalChanged(_m_Horizontal, timestep);
+			if (fieldAltered != null) fieldAltered("m_Horizontal", _m_Horizontal, timestep);
+		}
+		[ForgeGeneratedField]
+		private float _m_Vertical;
+		public event FieldEvent<float> m_VerticalChanged;
+		public InterpolateFloat m_VerticalInterpolation = new InterpolateFloat() { LerpT = 0.15f, Enabled = true };
+		public float m_Vertical
+		{
+			get { return _m_Vertical; }
+			set
+			{
+				// Don't do anything if the value is the same
+				if (_m_Vertical == value)
+					return;
+
+				// Mark the field as dirty for the network to transmit
+				_dirtyFields[0] |= 0x8;
+				_m_Vertical = value;
+				hasDirtyFields = true;
+			}
+		}
+
+		public void Setm_VerticalDirty()
+		{
+			_dirtyFields[0] |= 0x8;
+			hasDirtyFields = true;
+		}
+
+		private void RunChange_m_Vertical(ulong timestep)
+		{
+			if (m_VerticalChanged != null) m_VerticalChanged(_m_Vertical, timestep);
+			if (fieldAltered != null) fieldAltered("m_Vertical", _m_Vertical, timestep);
+		}
+		[ForgeGeneratedField]
+		private bool _isRotatingLeft;
+		public event FieldEvent<bool> isRotatingLeftChanged;
+		public Interpolated<bool> isRotatingLeftInterpolation = new Interpolated<bool>() { LerpT = 0f, Enabled = false };
+		public bool isRotatingLeft
+		{
+			get { return _isRotatingLeft; }
+			set
+			{
+				// Don't do anything if the value is the same
+				if (_isRotatingLeft == value)
+					return;
+
+				// Mark the field as dirty for the network to transmit
+				_dirtyFields[0] |= 0x10;
+				_isRotatingLeft = value;
+				hasDirtyFields = true;
+			}
+		}
+
+		public void SetisRotatingLeftDirty()
+		{
+			_dirtyFields[0] |= 0x10;
+			hasDirtyFields = true;
+		}
+
+		private void RunChange_isRotatingLeft(ulong timestep)
+		{
+			if (isRotatingLeftChanged != null) isRotatingLeftChanged(_isRotatingLeft, timestep);
+			if (fieldAltered != null) fieldAltered("isRotatingLeft", _isRotatingLeft, timestep);
+		}
+		[ForgeGeneratedField]
+		private bool _isRotatingRight;
+		public event FieldEvent<bool> isRotatingRightChanged;
+		public Interpolated<bool> isRotatingRightInterpolation = new Interpolated<bool>() { LerpT = 0f, Enabled = false };
+		public bool isRotatingRight
+		{
+			get { return _isRotatingRight; }
+			set
+			{
+				// Don't do anything if the value is the same
+				if (_isRotatingRight == value)
+					return;
+
+				// Mark the field as dirty for the network to transmit
+				_dirtyFields[0] |= 0x20;
+				_isRotatingRight = value;
+				hasDirtyFields = true;
+			}
+		}
+
+		public void SetisRotatingRightDirty()
+		{
+			_dirtyFields[0] |= 0x20;
+			hasDirtyFields = true;
+		}
+
+		private void RunChange_isRotatingRight(ulong timestep)
+		{
+			if (isRotatingRightChanged != null) isRotatingRightChanged(_isRotatingRight, timestep);
+			if (fieldAltered != null) fieldAltered("isRotatingRight", _isRotatingRight, timestep);
+		}
 
 		protected override void OwnershipChanged()
 		{
@@ -88,6 +212,10 @@ namespace BeardedManStudios.Forge.Networking.Generated
 		{
 			positionInterpolation.current = positionInterpolation.target;
 			rotationInterpolation.current = rotationInterpolation.target;
+			m_HorizontalInterpolation.current = m_HorizontalInterpolation.target;
+			m_VerticalInterpolation.current = m_VerticalInterpolation.target;
+			isRotatingLeftInterpolation.current = isRotatingLeftInterpolation.target;
+			isRotatingRightInterpolation.current = isRotatingRightInterpolation.target;
 		}
 
 		public override int UniqueIdentity { get { return IDENTITY; } }
@@ -96,6 +224,10 @@ namespace BeardedManStudios.Forge.Networking.Generated
 		{
 			UnityObjectMapper.Instance.MapBytes(data, _position);
 			UnityObjectMapper.Instance.MapBytes(data, _rotation);
+			UnityObjectMapper.Instance.MapBytes(data, _m_Horizontal);
+			UnityObjectMapper.Instance.MapBytes(data, _m_Vertical);
+			UnityObjectMapper.Instance.MapBytes(data, _isRotatingLeft);
+			UnityObjectMapper.Instance.MapBytes(data, _isRotatingRight);
 
 			return data;
 		}
@@ -110,6 +242,22 @@ namespace BeardedManStudios.Forge.Networking.Generated
 			rotationInterpolation.current = _rotation;
 			rotationInterpolation.target = _rotation;
 			RunChange_rotation(timestep);
+			_m_Horizontal = UnityObjectMapper.Instance.Map<float>(payload);
+			m_HorizontalInterpolation.current = _m_Horizontal;
+			m_HorizontalInterpolation.target = _m_Horizontal;
+			RunChange_m_Horizontal(timestep);
+			_m_Vertical = UnityObjectMapper.Instance.Map<float>(payload);
+			m_VerticalInterpolation.current = _m_Vertical;
+			m_VerticalInterpolation.target = _m_Vertical;
+			RunChange_m_Vertical(timestep);
+			_isRotatingLeft = UnityObjectMapper.Instance.Map<bool>(payload);
+			isRotatingLeftInterpolation.current = _isRotatingLeft;
+			isRotatingLeftInterpolation.target = _isRotatingLeft;
+			RunChange_isRotatingLeft(timestep);
+			_isRotatingRight = UnityObjectMapper.Instance.Map<bool>(payload);
+			isRotatingRightInterpolation.current = _isRotatingRight;
+			isRotatingRightInterpolation.target = _isRotatingRight;
+			RunChange_isRotatingRight(timestep);
 		}
 
 		protected override BMSByte SerializeDirtyFields()
@@ -121,6 +269,14 @@ namespace BeardedManStudios.Forge.Networking.Generated
 				UnityObjectMapper.Instance.MapBytes(dirtyFieldsData, _position);
 			if ((0x2 & _dirtyFields[0]) != 0)
 				UnityObjectMapper.Instance.MapBytes(dirtyFieldsData, _rotation);
+			if ((0x4 & _dirtyFields[0]) != 0)
+				UnityObjectMapper.Instance.MapBytes(dirtyFieldsData, _m_Horizontal);
+			if ((0x8 & _dirtyFields[0]) != 0)
+				UnityObjectMapper.Instance.MapBytes(dirtyFieldsData, _m_Vertical);
+			if ((0x10 & _dirtyFields[0]) != 0)
+				UnityObjectMapper.Instance.MapBytes(dirtyFieldsData, _isRotatingLeft);
+			if ((0x20 & _dirtyFields[0]) != 0)
+				UnityObjectMapper.Instance.MapBytes(dirtyFieldsData, _isRotatingRight);
 
 			// Reset all the dirty fields
 			for (int i = 0; i < _dirtyFields.Length; i++)
@@ -163,6 +319,58 @@ namespace BeardedManStudios.Forge.Networking.Generated
 					RunChange_rotation(timestep);
 				}
 			}
+			if ((0x4 & readDirtyFlags[0]) != 0)
+			{
+				if (m_HorizontalInterpolation.Enabled)
+				{
+					m_HorizontalInterpolation.target = UnityObjectMapper.Instance.Map<float>(data);
+					m_HorizontalInterpolation.Timestep = timestep;
+				}
+				else
+				{
+					_m_Horizontal = UnityObjectMapper.Instance.Map<float>(data);
+					RunChange_m_Horizontal(timestep);
+				}
+			}
+			if ((0x8 & readDirtyFlags[0]) != 0)
+			{
+				if (m_VerticalInterpolation.Enabled)
+				{
+					m_VerticalInterpolation.target = UnityObjectMapper.Instance.Map<float>(data);
+					m_VerticalInterpolation.Timestep = timestep;
+				}
+				else
+				{
+					_m_Vertical = UnityObjectMapper.Instance.Map<float>(data);
+					RunChange_m_Vertical(timestep);
+				}
+			}
+			if ((0x10 & readDirtyFlags[0]) != 0)
+			{
+				if (isRotatingLeftInterpolation.Enabled)
+				{
+					isRotatingLeftInterpolation.target = UnityObjectMapper.Instance.Map<bool>(data);
+					isRotatingLeftInterpolation.Timestep = timestep;
+				}
+				else
+				{
+					_isRotatingLeft = UnityObjectMapper.Instance.Map<bool>(data);
+					RunChange_isRotatingLeft(timestep);
+				}
+			}
+			if ((0x20 & readDirtyFlags[0]) != 0)
+			{
+				if (isRotatingRightInterpolation.Enabled)
+				{
+					isRotatingRightInterpolation.target = UnityObjectMapper.Instance.Map<bool>(data);
+					isRotatingRightInterpolation.Timestep = timestep;
+				}
+				else
+				{
+					_isRotatingRight = UnityObjectMapper.Instance.Map<bool>(data);
+					RunChange_isRotatingRight(timestep);
+				}
+			}
 		}
 
 		public override void InterpolateUpdate()
@@ -179,6 +387,26 @@ namespace BeardedManStudios.Forge.Networking.Generated
 			{
 				_rotation = (Quaternion)rotationInterpolation.Interpolate();
 				//RunChange_rotation(rotationInterpolation.Timestep);
+			}
+			if (m_HorizontalInterpolation.Enabled && !m_HorizontalInterpolation.current.UnityNear(m_HorizontalInterpolation.target, 0.0015f))
+			{
+				_m_Horizontal = (float)m_HorizontalInterpolation.Interpolate();
+				//RunChange_m_Horizontal(m_HorizontalInterpolation.Timestep);
+			}
+			if (m_VerticalInterpolation.Enabled && !m_VerticalInterpolation.current.UnityNear(m_VerticalInterpolation.target, 0.0015f))
+			{
+				_m_Vertical = (float)m_VerticalInterpolation.Interpolate();
+				//RunChange_m_Vertical(m_VerticalInterpolation.Timestep);
+			}
+			if (isRotatingLeftInterpolation.Enabled && !isRotatingLeftInterpolation.current.UnityNear(isRotatingLeftInterpolation.target, 0.0015f))
+			{
+				_isRotatingLeft = (bool)isRotatingLeftInterpolation.Interpolate();
+				//RunChange_isRotatingLeft(isRotatingLeftInterpolation.Timestep);
+			}
+			if (isRotatingRightInterpolation.Enabled && !isRotatingRightInterpolation.current.UnityNear(isRotatingRightInterpolation.target, 0.0015f))
+			{
+				_isRotatingRight = (bool)isRotatingRightInterpolation.Interpolate();
+				//RunChange_isRotatingRight(isRotatingRightInterpolation.Timestep);
 			}
 		}
 
---
+++ 8-puzzle/Assets/Bearded Man Studios Inc/Scripts/Tutorial.cs
@@ -0,0 +1,432 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject Button1;
+    public GameObject Button2;
+    public GameObject Button3;
+    public GameObject Button4;
+    public GameObject Button5;
+    public GameObject Button6;
+    public GameObject Button7;
+    public GameObject Button8;
+    public GameObject ButtontoTutorialPictures;
+    public List<Sprite> sprites = new List<Sprite>();
+    public Image ToolTipTutorial;
+    public Text TutorialLogText1;
+    public Text TutorialLogText2;
+    public Text TutorialLogText3;
+    public Text TutorialLogText4;
+
+    public Image SuccessImage;
+    public Image TutorialInstructionImage;
+    public GameObject ExitTutorialInstructionButton;
+
+    public float fadeDuration = 1f;
+    float m_Timer;
+    public float displayImageDuration = 1f;
+    bool m_IsSuccess=false;
+
+    int nKey;
+    int Button16=0;
+    int Button24=0;
+    int Button38=0;
+    int Button57=0;
+    int TotalButtonOn=0;
+    int LastClickedButton=0;
+    float speed=1.0f;
+    float Timer;
+    bool TimerWorks;
+    public bool ButtontoTutorialOn;
+    string LogText1="";
+    string LogText2="";
+    string LogText3="";
+    string LogText4="";
+    char TempChar;
+    string TempString;
+    int NumberofTest=0;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        Button1.GetComponent<Image>().sprite=sprites[4];
+        Button2.GetComponent<Image>().sprite=sprites[4];
+        Button3.GetComponent<Image>().sprite=sprites[4];
+        Button4.GetComponent<Image>().sprite=sprites[4];
+        Button5.GetComponent<Image>().sprite=sprites[4];
+        Button6.GetComponent<Image>().sprite=sprites[4];
+        Button7.GetComponent<Image>().sprite=sprites[4];
+        Button8.GetComponent<Image>().sprite=sprites[4];
+        SuccessImage.enabled=false;
+        ButtontoTutorialOn=false;
+        ToolTipTutorial.enabled=false;
+        TimerWorks=false;
+        TutorialInstructionImage.enabled=false;
+        ExitTutorialInstructionButton.SetActive(false);
+//        ButtontoTutorialPictures.GetComponent<Button>().interactable=false;
+ /*       Right1.SetActive(false);
+        Right2.SetActive(false);
+        Right3.SetActive(false);
+        Right4.SetActive(false);        */
+    }
+    void Update() 
+    {
+        if(TimerWorks) {
+            Timer+=Time.deltaTime;
+            if(Timer>2) {
+                TimerWorks=false;
+                ToolTipTutorial.enabled=false;
+            }
+        }
+        if(m_IsSuccess==true) {
+            SuccessImage.enabled=true;
+            EndTutorial(SuccessImage);
+        } else {
+            if(LastClickedButton!=0) {
+                if(LastClickedButton==1) {
+                    //transform.rotation=Quaternion.identity;
+                    Button1.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==2) {
+                    //transform.rotation=Quaternion.identity;
+                    Button2.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==3) {
+                    //transform.rotation=Quaternion.identity;
+                    Button3.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==4) {
+                    //transform.rotation=Quaternion.identity;
+                    Button4.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==5) {
+                    //transform.rotation=Quaternion.identity;
+                    Button5.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==6) {
+                    //transform.rotation=Quaternion.identity;
+                    Button6.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==7) {
+                    //transform.rotation=Quaternion.identity;
+                    Button7.transform.Rotate(Vector3.forward*speed);
+                }
+                if(LastClickedButton==8) {
+                    //transform.rotation=Quaternion.identity;
+                    Button8.transform.Rotate(Vector3.forward*speed);
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    public void PressKey(int nKey)
+    {
+        if(nKey!=LastClickedButton) {
+            switch (nKey) 
+            {
+                case 1:
+                    if(Button16==0) Button16=1;
+                    else Button16=0;
+                    init_ButtonRotation();
+                    LastClickedButton=1;
+                    break;
+                case 2:
+                    if(Button24==0) Button24=1;
+                    else Button24=0;
+                    init_ButtonRotation();
+                    LastClickedButton=2;
+                    break;
+                case 3:
+                    if(Button38==0) Button38=1;
+                    else Button38=0;
+                    init_ButtonRotation();
+                    LastClickedButton=3;
+                    break;
+                case 4:
+                    if(Button24==0) Button24=1;
+                    else Button24=0;
+                    init_ButtonRotation();
+                    LastClickedButton=4;
+                    break;
+                case 5:
+                    if(Button57==0) Button57=1;
+                    else Button57=0;
+                    init_ButtonRotation();
+                    LastClickedButton=5;
+                    break;
+                case 6:
+                    if(Button16==0) Button16=1;
+                    else Button16=0;
+                    init_ButtonRotation();
+                    LastClickedButton=6;
+                    break;
+                case 7:
+                    if(Button57==0) Button57=1;
+                    else Button57=0;
+                    init_ButtonRotation();
+                    LastClickedButton=7;
+                    break;
+                case 8:
+                    if(Button38==0) Button38=1;
+                    else Button38=0;
+                    init_ButtonRotation();
+                    LastClickedButton=8;
+                    break;
+                case 9:
+                    ChangeScenetoTutorialPicture();
+                    break;
+                case 10:
+                    ShowTutorialInstruction();
+                    break;
+                case 11:
+                    QuitTutorialInstruction();
+                    break;
+            }
+        }
+        if(nKey<=8) {
+            TotalButtonOn=Button16+Button24+Button38+Button57;
+            if(TotalButtonOn==0) {
+                TempChar=(char)(LastClickedButton+'0');
+                TempString=TempChar.ToString();
+                PrintLog();
+    /*            Button1.GetComponent<Image>().sprite=sprites[0];
+                Button2.GetComponent<Image>().sprite=sprites[0];
+                Button3.GetComponent<Image>().sprite=sprites[0];
+                Button4.GetComponent<Image>().sprite=sprites[0];
+                Button5.GetComponent<Image>().sprite=sprites[0];
+                Button6.GetComponent<Image>().sprite=sprites[0];
+                Button7.GetComponent<Image>().sprite=sprites[0];
+                Button8.GetComponent<Image>().sprite=sprites[0];*/
+            }
+            if(TotalButtonOn==1) {
+                TempChar=(char)(LastClickedButton+'0');
+                TempString=TempChar.ToString();
+                PrintLog();
+                /*
+                Button1.GetComponent<Image>().sprite=sprites[1];
+                Button2.GetComponent<Image>().sprite=sprites[1];
+                Button3.GetComponent<Image>().sprite=sprites[1];
+                Button4.GetComponent<Image>().sprite=sprites[1];
+                Button5.GetComponent<Image>().sprite=sprites[1];
+                Button6.GetComponent<Image>().sprite=sprites[1];
+                Button7.GetComponent<Image>().sprite=sprites[1];
+                Button8.GetComponent<Image>().sprite=sprites[1];*/
+            }
+            if(TotalButtonOn==2) {
+                TempChar=(char)(LastClickedButton+'0');
+                TempString=TempChar.ToString();
+                PrintLog();
+                /*
+                Button1.GetComponent<Image>().sprite=sprites[2];
+                Button2.GetComponent<Image>().sprite=sprites[2];
+                Button3.GetComponent<Image>().sprite=sprites[2];
+                Button4.GetComponent<Image>().sprite=sprites[2];
+                Button5.GetComponent<Image>().sprite=sprites[2];
+                Button6.GetComponent<Image>().sprite=sprites[2];
+                Button7.GetComponent<Image>().sprite=sprites[2];
+                Button8.GetComponent<Image>().sprite=sprites[2];*/
+                }
+            if(TotalButtonOn==3) {
+                TempChar=(char)(LastClickedButton+'0');
+                TempString=TempChar.ToString();
+                PrintLog();
+                /*
+                Button1.GetComponent<Image>().sprite=sprites[3];
+                Button2.GetComponent<Image>().sprite=sprites[3];
+                Button3.GetComponent<Image>().sprite=sprites[3];
+                Button4.GetComponent<Image>().sprite=sprites[3];
+                Button5.GetComponent<Image>().sprite=sprites[3];
+                Button6.GetComponent<Image>().sprite=sprites[3];
+                Button7.GetComponent<Image>().sprite=sprites[3];
+                Button8.GetComponent<Image>().sprite=sprites[3];*/
+            }
+            if(TotalButtonOn==4) {
+                TempChar=(char)(LastClickedButton+'0');
+                TempString=TempChar.ToString();
+                PrintLog();
+                /*
+                Button1.GetComponent<Image>().sprite=sprites[4];
+                Button2.GetComponent<Image>().sprite=sprites[4];
+                Button3.GetComponent<Image>().sprite=sprites[4];
+                Button4.GetComponent<Image>().sprite=sprites[4];
+                Button5.GetComponent<Image>().sprite=sprites[4];
+                Button6.GetComponent<Image>().sprite=sprites[4];
+                Button7.GetComponent<Image>().sprite=sprites[4];
+                Button8.GetComponent<Image>().sprite=sprites[4];*/
+                m_IsSuccess=true;
+                ButtontoTutorialOn=true;
+            }
+        }
+    }
+
+    void init_ButtonRotation()
+    {
+        switch(LastClickedButton) {
+            case 1:
+                Button1.transform.rotation = Quaternion.identity;
+                break;
+            case 2:
+                Button2.transform.rotation = Quaternion.identity;
+                break;
+            case 3:
+                Button3.transform.rotation = Quaternion.identity;
+                break;
+            case 4:
+                Button4.transform.rotation = Quaternion.identity;
+                break;
+            case 5:
+                Button5.transform.rotation = Quaternion.identity;
+                break;
+            case 6:
+                Button6.transform.rotation = Quaternion.identity;
+                break;
+            case 7:
+                Button7.transform.rotation = Quaternion.identity;
+                break;
+            case 8:
+                Button8.transform.rotation = Quaternion.identity;
+                break;
+
+        }
+    }
+    void EndTutorial(Image image) {
+        m_Timer += Time.deltaTime;
+        image.color = new Color(image.color.r,image.color.g,image.color.b, m_Timer / fadeDuration);
+        if(m_Timer>fadeDuration + displayImageDuration) {
+            m_IsSuccess=false;
+            SuccessImage.enabled=false;
+        }
+    }
+
+    void ChangeScenetoTutorialPicture() {
+        if(ButtontoTutorialOn) {                                    //이거 public으로 하면 못받나???
+            SceneManager.LoadScene("TutorialPicture");
+        } else {
+            ShowToolTip();
+        }
+    }
+    void ShowToolTip() {
+        ToolTipTutorial.enabled=true;
+        Timer=0;
+        TimerWorks=true;
+    }
+    void ShowTutorialInstruction() {
+        TutorialInstructionImage.enabled=true;
+        ExitTutorialInstructionButton.SetActive(true);
+    }
+    void QuitTutorialInstruction() {
+        TutorialInstructionImage.enabled=false;
+        ExitTutorialInstructionButton.SetActive(false);
+    }
+    void PrintLog() {
+        NumberofTest++;
+        int divi=NumberofTest/11;
+        if(divi==0) {
+            if(TotalButtonOn==0) {
+                LogText1=LogText1+"\n"+TempString+"   0/4";
+            } else if(TotalButtonOn==1) {
+                LogText1=LogText1+"\n"+TempString+"   1/4";
+            } else if(TotalButtonOn==2) {
+                LogText1=LogText1+"\n"+TempString+"   2/4";
+            } else if(TotalButtonOn==3) {
+                LogText1=LogText1+"\n"+TempString+"   3/4";
+            } else if(TotalButtonOn==4) {
+                LogText1=LogText1+"\n"+TempString+"   4/4";
+            }
+            TutorialLogText1.GetComponent<Text>().text=LogText1;
+        } else if(divi==1) {
+            if(TotalButtonOn==0) {
+                LogText2=LogText2+"\n"+TempString+"   0/4";
+            } else if(TotalButtonOn==1) {
+                LogText2=LogText2+"\n"+TempString+"   1/4";
+            } else if(TotalButtonOn==2) {
+                LogText2=LogText2+"\n"+TempString+"   2/4";
+            } else if(TotalButtonOn==3) {
+                LogText2=LogText2+"\n"+TempString+"   3/4";
+            } else if(TotalButtonOn==4) {
+                LogText2=LogText2+"\n"+TempString+"   4/4";
+            }
+            TutorialLogText2.GetComponent<Text>().text=LogText2;
+        } else if(divi==2) {
+            if(TotalButtonOn==0) {
+                LogText3=LogText3+"\n"+TempString+"   0/4";
+            } else if(TotalButtonOn==1) {
+                LogText3=LogText3+"\n"+TempString+"   1/4";
+            } else if(TotalButtonOn==2) {
+                LogText3=LogText3+"\n"+TempString+"   2/4";
+            } else if(TotalButtonOn==3) {
+                LogText3=LogText3+"\n"+TempString+"   3/4";
+            } else if(TotalButtonOn==4) {
+                LogText3=LogText3+"\n"+TempString+"   4/4";
+            }
+            TutorialLogText3.GetComponent<Text>().text=LogText3;
+        } else if(divi==3) {
+            if(TotalButtonOn==0) {
+                LogText4=LogText4+"\n"+TempString+"   0/4";
+            } else if(TotalButtonOn==1) {
+                LogText4=LogText4+"\n"+TempString+"   1/4";
+            } else if(TotalButtonOn==2) {
+                LogText4=LogText4+"\n"+TempString+"   2/4";
+            } else if(TotalButtonOn==3) {
+                LogText4=LogText4+"\n"+TempString+"   3/4";
+            } else if(TotalButtonOn==4) {
+                LogText4=LogText4+"\n"+TempString+"   4/4";
+            }
+            TutorialLogText4.GetComponent<Text>().text=LogText4;
+        }
+    }
+}
+    /*
+    Right1.SetActive(false);
+    Right2.SetActive(false);
+    Right3.SetActive(false);
+    Right4.SetActive(false);
+    switch (TotalButtonOn) {
+        case 4:
+            Right4.SetActive(true);
+            Right3.SetActive(true);
+            Right2.SetActive(true);
+            Right1.SetActive(true);
+            break;
+        case 3:
+            Right3.SetActive(true);
+            Right2.SetActive(true);
+            Right1.SetActive(true);
+            break;
+        case 2:
+            Right2.SetActive(true);
+            Right1.SetActive(true);
+            break;
+        case 1:
+            Right1.SetActive(true);
+            break;
+    }
+    }*/
+/*
+    void CastRay() 
+    {
+        target=null;
+        Vector2 pos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit=Physics2D.Raycast(pos,Vector2.zero,0f);
+
+        if(hit.collider !=null) {
+            target=hit.collider.gameObject;
+        }
+    }
+    private GameObject GetClickedobject() 
+    {
+        RaycastHit hit;
+        GameObject target=null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        if( true == (Physics.Raycast(ray.origin, ray.direction * 10, out hit))) 
+        {
+            target = hit.collider.gameObject; 
+        }
+        return target;
+    }*/
+
