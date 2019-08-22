3487d3c2300efda56aadfc7c82840b3ea6495e7a
Nyso <justin0907@snu.ac.kr>
2019-08-23 05:42:41 +0900

    added crates in scenefog

--- 8-puzzle/Assets/Scripts/Crate.cs
+++ 8-puzzle/Assets/Scripts/Crate.cs
@@ -26,7 +26,7 @@ public class Crate : CrateBehavior
     {
         for(int i = 0; i <= 20; i++)
         {
-            CrateLid.rotation = Quaternion.Slerp(Quaternion.Euler(-90f, 0f, 0f), Quaternion.Euler(-0f, 0f, 0f), i / 20f);
+            CrateLid.rotation = Quaternion.Slerp(Quaternion.Euler(-90f, 0f, 0f), Quaternion.Euler(0f, 0f, 0f), i / 20f);
             yield return null;
         }
 //        yield return new WaitForSeconds(stopTime);
